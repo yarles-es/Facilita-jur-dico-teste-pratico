e96d6618e04665778089bd8240fee90e8882d243
Yarles de Andrade Espirito Santo <yarles100@gmail.com>
2024-03-11 10:00:24 -0300

    fix: corrigido para os inputs de filtro aparecerem apenas quando houver algum cliente

--- frontEnd/src/components/Table/index.tsx
+++ frontEnd/src/components/Table/index.tsx
@@ -43,58 +43,60 @@ const Table = ({ clients }: Props) => {
           <th>Coordenada X</th>
           <th>Coordenada Y</th>
         </tr>
-        <tr>
-          <th>
-            <input
-              type="text"
-              name="name"
-              value={filter.name}
-              onChange={handleFilterChange}
-              placeholder="Filtrar por Nome"
-              className={styles.filterInput}
-            />
-          </th>
-          <th>
-            <input
-              type="text"
-              name="email"
-              value={filter.email}
-              onChange={handleFilterChange}
-              placeholder="Filtrar por Email"
-              className={styles.filterInput}
-            />
-          </th>
-          <th>
-            <input
-              type="text"
-              name="phone"
-              value={filter.phone}
-              onChange={handleFilterChange}
-              placeholder="Filtrar por Telefone"
-              className={styles.filterInput}
-            />
-          </th>
-          <th>
-            <input
-              type="text"
-              name="coord_x"
-              value={filter.coord_x}
-              onChange={handleFilterChange}
-              placeholder="Filtrar por Coordenada X"
-              className={styles.filterInput}
-            />
-          </th>
-          <th>
-            <input
-              type="text"
-              name="coord_y"
-              value={filter.coord_y}
-              onChange={handleFilterChange}
-              placeholder="Filtrar por Coordenada Y"
-              className={styles.filterInput}
-            />
-          </th>
-        </tr>
+        {clients.length > 0 && (
+          <tr>
+            <th>
+              <input
+                type="text"
+                name="name"
+                value={filter.name}
+                onChange={handleFilterChange}
+                placeholder="Filtrar por Nome"
+                className={styles.filterInput}
+              />
+            </th>
+            <th>
+              <input
+                type="text"
+                name="email"
+                value={filter.email}
+                onChange={handleFilterChange}
+                placeholder="Filtrar por Email"
+                className={styles.filterInput}
+              />
+            </th>
+            <th>
+              <input
+                type="text"
+                name="phone"
+                value={filter.phone}
+                onChange={handleFilterChange}
+                placeholder="Filtrar por Telefone"
+                className={styles.filterInput}
+              />
+            </th>
+            <th>
+              <input
+                type="text"
+                name="coord_x"
+                value={filter.coord_x}
+                onChange={handleFilterChange}
+                placeholder="Filtrar por Coordenada X"
+                className={styles.filterInput}
+              />
+            </th>
+            <th>
+              <input
+                type="text"
+                name="coord_y"
+                value={filter.coord_y}
+                onChange={handleFilterChange}
+                placeholder="Filtrar por Coordenada Y"
+                className={styles.filterInput}
+              />
+            </th>
+          </tr>
+        )}
       </thead>
       <tbody>
         {filteredClients.map((client) => (
